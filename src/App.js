04dc8b082dee8ed578209730c540ccf76c237a4c
Андрей <andreyqq@gmail.com>
2020-10-10 10:55:04 +0400

    feat: blueprintjs;

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Switch,
   Route,
@@ -9,6 +9,9 @@ import Post from './pages/Post';
 
 
 const App = () => {
+  const [posts, setPosts] = useState([])
+
+
   return (
     <div>
       <Switch>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react';
 import logo from '../logo.svg';
 import axios from 'axios';
-import {
-  Link,
-} from "react-router-dom";
-import '../App.css';
-
+import { Button, Intent, Spinner } from "@blueprintjs/core";
 
 
 class Home extends Component {
@@ -16,22 +12,22 @@ class Home extends Component {
     };
   }
 
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => this.setState({ posts: response.data }))
-    }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => this.setState({ posts: response.data }))
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Spinner intent={Intent.PRIMARY} />
           <img src={logo} className="App-logo" alt="logo" />
             {this.state.posts.map(post => (
               <div key={post.id}>
-                <Link to={`/post/${post.id}`}>
                     <h1>{post.title}</h1>
                     <p>{post.body}</p>
-                </Link>
+                    <Button onClick={() => this.props.history.push(`/post/${post.id}`)} intent={ Intent.SUCCESS } >View Post</Button>
               </div>
             ))}
         </header>
